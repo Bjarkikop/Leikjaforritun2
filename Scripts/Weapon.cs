d50942eedcd55b5472d9580153eceeb42225ed83
Bjarkikop <42611493+Bjarkikop@users.noreply.github.com>
2020-12-02 10:55:40 +0000

    kóði

---
+++ Scripts/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    //ef ýtt er á músina skýtur leikmaður
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    //fall sem lætur leikmann skjóta
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
---
+++ Scripts/bullet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    //gef hraða á bullet
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    //hversu mikið damage bullet gerir
+    public int damage = 40;
+    void Start()
+    {
+        rb.velocity = transform.right * speed; 
+    }
+
+    //sér hvort bullet hittir óvin
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+        BossHealth boss = collision.GetComponent<BossHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        if(boss != null)
+        {
+            boss.TakeDamage(damage);
+        }
+        
+    }
+
+
+}
